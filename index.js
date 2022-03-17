2d2d6106b3f30f3e4e0e7a540bbf8609fbde8f9c
Kristina Diaz <kristina.diaz84@gmail.com>
2022-03-17 15:56:08 -0600

    added more js functionality

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-let count = 20
-let countEl = document.getElementById("count-el").textContent = count
+// 0 meters = 0.000 feet | 0 feet = 0.00 meters
+// 0 liters = 0.000 gallons | 0 gallons = 0.00 liters
+// 0 kilos = 0.00 pounds | 0 pounds = 0.000 kilos
+
+let number = 20
+let numberEl = document.getElementById("number-el").textContent = number
 let meterEl = document.getElementById("meter-el")
+// console.log(meterEl)
 
 function meterToFeet() {
-    let sum = count * 3.281;
-    meterEl.textContent = sum
+    let sum = numberEl * 3.281
+    meterEl.textContent = "20 meters = " + sum.toFixed(3) + " feet | "
 }
+
+function feetToMeter() {
+    let sum = numberEl / 3.281
+    meterEl = "20 feet = " + sum.toFixed(3) + " meters"
+}
+
+function litersToGallons() {}
+
+function gallonsToLiters() {}
+
+function kilosToPounds() {}
+
+function poundsToKilos() {}
