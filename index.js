1fa1ef5ab54aeed062640b9e8cd7c97e4c27a021
Kristina Diaz <kristina.diaz84@gmail.com>
2022-03-18 10:08:04 -0600

    able to get volume conversion working

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-let numberEl = prompt('Please enter a number: ')
-document.getElementById('number-el').textContent = numberEl
+let numberEl = prompt('Please enter a number: ');
+document.getElementById('number-el').textContent = numberEl;
 
-let lengthEl = document.getElementById('length-el')
-let volumeEl = document.getElementById('volume-el')
-let massEl = document.getElementById('mass-el')
+let lengthEl = document.getElementById('length-el');
+let volumeEl = document.getElementById('volume-el');
+let massEl = document.getElementById('mass-el');
 
 function length() {
-    let feet = numberEl * 3.281
-    let meter = numberEl / 3.281
-    lengthEl.textContent = `${numberEl} meters = ${feet.toFixed(3)} feet | ${numberEl} feet = ${meter.toFixed(3)} meters`
-}
+    let feet = numberEl * 3.281;
+    let meter = numberEl / 3.281;
+    lengthEl.textContent = `${numberEl} meters = ${feet.toFixed(3)} feet | ${numberEl} feet = ${meter.toFixed(3)} meters`;
+};
 
-function volume() {}
+function volume() {
+    let liters = numberEl / 3.785;
+    let gallons = numberEl * 3.785;
+    volumeEl.textContent = `${numberEl} liters = ${gallons.toFixed(3)} | ${numberEl} gallons = ${liters.toFixed(3)} liters`;
+};
 
-function mass() {}
+function mass() {
+    // kilos to pounds
+    // pounds to kilos
+};
 
-length()
+length();
+volume();
